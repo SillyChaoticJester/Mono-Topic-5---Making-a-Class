d09ef86880cc589089d0d923278e5f55fc5c453b
Miracle McMahon <339663114@GoTVDSB.ca>
2025-04-14 14:36:57 -0400

    Why is it doing weird things???

---
+++ Ghost.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mono_Topic_5___Making_a_Class
+{
+    public class Ghost
+    {
+        private List<Texture2D> _textures;
+        private Vector2 _speed;
+        private Rectangle _location;
+        private int _textureIndex;
+    }
+
+    
+
+}
